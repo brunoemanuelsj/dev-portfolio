6e6ae1d9df7b1bbaee45ef18f94afe731ec0c85e
Bruno Jaime <emanuelbrunosj@gmail.com>
2025-04-17 00:45:21 -0300

    teste pipeline

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/dev-portifolio/',
+  base: '/dev-portfolio/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
